6124ea234c4ce0d625d12d8c90e4cf6f118b57bf
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2023-10-09 21:45:50 +0630

    Dynamic component with React built-in object props'

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
+    </>
+  );
+}
+
+const App = () => {
+  return (
+    <div className="App">
+      <Person name={'John'} lastName={'Doe'} age={25} />
+      <Person name={'Mary'} lastName={'Doe'} age={24} />
+    </div>
+  );
+}
+
+export default App;
